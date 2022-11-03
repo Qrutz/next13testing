755bfbc0d8c1aae6376df13f3483a7bed20234de
Qrutz <Johan-Sandgren@hotmail.com>
2022-11-03 12:04:09 +0100

    app folder

---
+++ app/Character/[charid]/page.tsx
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import {usePathname} from 'next/navigation';
+
+
+async function getData() {
+  // get the url params 
+ 
+
+
+  const res = await fetch(`https://rickandmortyapi.com/api/character/235{params.id}`);
+
+  return res.json();
+}
+
+
+export default async function Page({params} : {
+    params: {
+        charid: string
+    }
+}) {
+
+
+
+  const r = await fetch(`https://rickandmortyapi.com/api/character/${params.charid}`).then(res => res.json());
+
+  return (
+    <div>
+
+     <h2> {r.name} </h2>
+    <img src={r.image} alt="" />
+    </div>
+  )
+}
